93ea003fdfa1e5526e90d131c03eeba78f811dc7
akash20042 <akashprinsh728@gmail.com>
2024-06-25 13:48:15 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,4 @@
+const head =  React.createElement("div",{ id:"parent"} ,
+React.createElement("h2",{ id:"child"} ," hi react bhai"));
+  const root =  ReactDOM.createRoot( document. getElementById("root"));
+  root.render(head);
